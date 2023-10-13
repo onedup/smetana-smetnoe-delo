ec32164f35b05e87d8d5c357f6ad63cb686f917a
onedup <nikulichevmikhail@gmail.com>
2023-10-13 14:39:46 +0400

    Update 2023-10-13T10:39:37.418Z

--- src/js/components/burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector(".burger");
-const menu = document.querySelector(".nav");
-
-burger.addEventListener("click", (e) => {
-  burger.classList.toggle("burger--active"), 
-  menu.classList.toggle("nav--active");
-});
